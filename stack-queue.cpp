a031c06652c1a6e03da1af9279b24cbdb3e96c79
Yar384 <aroslavbelka83@gmail.com>
2025-11-15 12:03:52 +0200

    Update stack-queue.cpp

--- stack-queue.cpp
+++ stack-queue.cpp
@@ -1,111 +1,34 @@
 #include <iostream>
-#include <fstream>
+#include <string>
 using namespace std;
 
-class Shape {
-public:
-    virtual void Show() = 0;
-    virtual void Save(ofstream& file) = 0;
-    virtual void Load(ifstream& file) = 0;
-    virtual ~Shape() {}
-};
+//1
+void printType(void* ptr, const string& typeName) {
+    cout << "Type: " << typeName << endl;
+}
 
-class Square : public Shape {
-private:
-    double side;
-public:
-    Square(double s = 0) : side(s) {}
-
-    void Show() override {
-        cout << "Square, side = " << side << endl;
-    }
-
-    void Save(ofstream& file) override {
-        file << "Square " << side << endl;
-    }
-
-    void Load(ifstream& file) override {
-        file >> side;
-    }
-};
-
-class Rectangle : public Shape {
-private:
-    double width, height;
-public:
-    Rectangle(double w = 0, double h = 0) : width(w), height(h) {}
-
-    void Show() override {
-        cout << "Rectangle, width = " << width << ", height = " << height << endl;
-    }
-
-    void Save(ofstream& file) override {
-        file << "Rectangle " << width << " " << height << endl;
-    }
-
-    void Load(ifstream& file) override {
-        file >> width >> height;
-    }
-};
-
-class Circle : public Shape {
-private:
-    double radius;
-public:
-    Circle(double r = 0) : radius(r) {}
-
-    void Show() override {
-        cout << "Circle, radius = " << radius << endl;
-    }
-
-    void Save(ofstream& file) override {
-        file << "Circle " << radius << endl;
-    }
-
-    void Load(ifstream& file) override {
-        file >> radius;
-    }
-};
+//2
+void printChar(int value) {
+    char* ch = reinterpret_cast<char*>(&value);
+    cout << "Character: " << *ch << endl;
+}
 
 int main() {
-    Shape* shapes[3];
-    shapes[0] = new Square(5);
-    shapes[1] = new Rectangle(3, 6);
-    shapes[2] = new Circle(4);
+    //1
+    int a = 10;
+    double b = 3.14;
+    char c = 'A';
 
-    ofstream out("shapes.txt");
-    for (int i = 0; i < 3; i++) {
-        shapes[i]->Save(out);
-    }
-    out.close();
+    printType(&a, "int");
+    printType(&b, "double");
+    printType(&c, "char");
 
-    for (int i = 0; i < 3; i++) {
-        delete shapes[i];
-    }
+    //2
+    int x;
+    cout << "Enter number: ";
+    cin >> x;
 
-    Shape* loaded[3];
-    ifstream in("shapes.txt");
-    for (int i = 0; i < 3; i++) {
-        string type;
-        in >> type;
-        if (type == "Square") {
-            loaded[i] = new Square();
-        }
-        else if (type == "Rectangle") {
-            loaded[i] = new Rectangle();
-        }
-        else if (type == "Circle") {
-            loaded[i] = new Circle();
-        }
-        loaded[i]->Load(in);
-    }
-    in.close();
-
-    cout << "Loaded shapes:" << endl;
-    for (int i = 0; i < 3; i++) {
-        loaded[i]->Show();
-        delete loaded[i];
-    }
+    printChar(x);
 
     return 0;
 }
