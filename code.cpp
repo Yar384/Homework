978ba47d1db178d324a1912037b0eb62bfc664e8
Yar384 <aroslavbelka83@gmail.com>
2025-12-07 10:14:50 +0200

    Refactor GuessGame into audio classes

--- code.cpp
+++ code.cpp
@@ -1,67 +1,107 @@
 #include <iostream>
-#include <fstream>
-#include <random>
+#include <string>
 using namespace std;
 
-class GuessGame {
+class Audio {
+public:
+    virtual void Play() = 0; // чисто віртуальний метод
+    virtual ~Audio() {}
+};
+
+class Song : public Audio {
 private:
-    int secret;
-    int attempts;
-    ofstream log;
+    string title;
+    string artist;
 
 public:
-    GuessGame(const string& filename) : attempts(0) {
-        // Генератор справжніх випадкових чисел
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_int_distribution<int> dist(1, 500);
+    Song(const string& t, const string& a) : title(t), artist(a) {}
 
-        secret = dist(gen);
-
-        log.open(filename, ios::app);
-        if (!log.is_open()) {
-            cerr << "Помилка відкриття файлу!\n";
-        }
-        log << "Загадане число: " << secret << "\n";
+    void Play() override {
+        cout << "Playing song: " << title << " by " << artist << "\n";
     }
+};
 
-    ~GuessGame() {
-        if (log.is_open()) {
-            log << "-------------------------\n";
-            log.close();
-        }
+class Podcast : public Audio {
+private:
+    string host;
+    string topic;
+
+public:
+    Podcast(const string& h, const string& t) : host(h), topic(t) {}
+
+    void Play() override {
+        cout << "Podcast on " << topic << " hosted by " << host << "\n";
     }
+};
 
-    void play() {
-        int guess;
-        cout << "Вгадайте число від 1 до 500.\n";
+class Audiobook : public Audio {
+private:
+    string bookTitle;
+    string author;
+    string voice;
 
-        while (true) {
-            cout << "Ваш варіант: ";
-            cin >> guess;
+public:
+    Audiobook(const string& bt, const string& a, const string& v)
+        : bookTitle(bt), author(a), voice(v) {}
 
-            attempts++;
-            log << "Спроба " << attempts << ": " << guess;
-
-            if (guess > secret) {
-                cout << "Загадане число менше.\n";
-                log << " (більше)\n";
-            }
-            else if (guess < secret) {
-                cout << "Загадане число більше.\n";
-                log << " (менше)\n";
-            }
-            else {
-                cout << "Вітаю! Ви вгадали число за " << attempts << " спроб!\n";
-                log << " (вгадано)\n";
-                break;
-            }
-        }
+    void Play() override {
+        cout << "Listening to audiobook: " << bookTitle
+             << " by " << author
+             << ". Read by " << voice << ".\n";
     }
 };
 
 int main() {
-    GuessGame game("game_log.txt");
-    game.play();
+    int choice;
+    Audio* audio = nullptr;
+
+    cout << "Select audio type:\n";
+    cout << "1 - Song\n";
+    cout << "2 - Podcast\n";
+    cout << "3 - Audiobook\n";
+    cout << "Your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        string title, artist;
+
+        cout << "Enter song title: ";
+        getline(cin, title);
+        cout << "Enter artist: ";
+        getline(cin, artist);
+
+        audio = new Song(title, artist);
+    }
+    else if (choice == 2) {
+        string host, topic;
+
+        cout << "Enter podcast host: ";
+        getline(cin, host);
+        cout << "Enter podcast topic: ";
+        getline(cin, topic);
+
+        audio = new Podcast(host, topic);
+    }
+    else if (choice == 3) {
+        string bookTitle, author, voice;
+
+        cout << "Enter audiobook title: ";
+        getline(cin, bookTitle);
+        cout << "Enter author: ";
+        getline(cin, author);
+        cout << "Enter voice actor: ";
+        getline(cin, voice);
+
+        audio = new Audiobook(bookTitle, author, voice);
+    }
+    else {
+        cout << "Invalid choice!\n";
+        return 0;
+    }
+
+    cout << "\n--- Playing ---\n";
+    audio->Play();
+
+    delete audio;
     return 0;
 }
